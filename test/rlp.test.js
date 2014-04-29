de94b2dffcffe565303ab18a1871e42fc97fab37
ethers <ethereum@outlook.com>
2014-04-28 17:23:56 -0700

    rlp namecoin test

--- test/rlp.test.js
+++ test/rlp.test.js
@@ -53,6 +53,14 @@ describe('rlp', function(){
       var res = rlp.encode(data);
       res.should.equal(exp);
     })
+
+    it('should encode namecoin', function(){
+      var namecoinHex = 'f8388085e8d4a510008227108080ab600035566300000021596020356000355760015b525b54602052f2630000002b5860005b525b54602052f2';
+      var data = util.decodeHex(namecoinHex);
+      var exp = "\xb8:\xf88\x80\x85\xe8\xd4\xa5\x10\x00\x82'\x10\x80\x80\xab`\x005Vc\x00\x00\x00!Y` 5`\x005W`\x01[R[T` R\xf2c\x00\x00\x00+X`\x00[R[T` R\xf2";
+      var res = rlp.encode(data);
+      res.should.equal(exp);
+    })
   })
 
   describe('#decode', function(){
@@ -61,6 +69,16 @@ describe('rlp', function(){
       var exp = 'cat';
       var res = rlp.decode(data);
       res.should.equal(exp);
+    })
+
+    it('should decode namecoin', function(){
+      var namecoinRlp = "\xb8:\xf88\x80\x85\xe8\xd4\xa5\x10\x00\x82'\x10\x80\x80\xab`\x005Vc\x00\x00\x00!Y` 5`\x005W`\x01[R[T` R\xf2c\x00\x00\x00+X`\x00[R[T` R\xf2";
+      var namecoinHex = 'f8388085e8d4a510008227108080ab600035566300000021596020356000355760015b525b54602052f2630000002b5860005b525b54602052f2';
+      var exp = "\xf88\x80\x85\xe8\xd4\xa5\x10\x00\x82'\x10\x80\x80\xab`\x005Vc\x00\x00\x00!Y` 5`\x005W`\x01[R[T` R\xf2c\x00\x00\x00+X`\x00[R[T` R\xf2";
+
+      var res = rlp.decode(namecoinRlp);
+      res.should.equal(exp);
+      util.encodeHex(res).should.equal(namecoinHex);
     })
   })
 })
