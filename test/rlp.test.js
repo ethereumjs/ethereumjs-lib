009792779848bcc34cbbec6c2fdbf9ee2ea407bc
ethers <ethereum@outlook.com>
2014-04-28 17:11:15 -0700

    rlp encode tests; todo: integers

--- test/rlp.test.js
+++ test/rlp.test.js
@@ -4,6 +4,57 @@ var rlp = require('../src/rlp');
 var util = require('../src/util');
 
 describe('rlp', function(){
+  describe('#encode', function(){
+    it('should encode dog', function(){
+      var data = 'dog';
+      var exp = '\x83dog';
+      var res = rlp.encode(data);
+      res.should.equal(exp);
+    })
+
+    it('should encode [cat, dog]', function(){
+      var data = ['cat', 'dog'];
+      var exp = '\xc8\x83cat\x83dog';
+      var res = rlp.encode(data);
+      res.should.equal(exp);
+    })
+
+    it('should encode empty string', function(){
+      var data = '';
+      var exp = '\x80';
+      var res = rlp.encode(data);
+      res.should.equal(exp);
+    })
+
+    it('should encode empty []', function(){
+      var data = [];
+      var exp = '\xc0';
+      var res = rlp.encode(data);
+      res.should.equal(exp);
+    })
+
+    it.skip('should encode integer 15', function(){
+      var data = 15;
+      var exp = '\x0f';
+      var res = rlp.encode(data);
+      res.should.equal(exp);
+    })
+
+    it('should encode set theoretical representation of two', function(){
+      var data = [ [], [[]], [ [], [[]] ] ];
+      var exp = '\xc7\xc0\xc1\xc0\xc3\xc0\xc1\xc0';
+      var res = rlp.encode(data);
+      res.should.equal(exp);
+    })
+
+    it('should encode string', function(){
+      var data = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit';
+      var exp = '\xb88Lorem ipsum dolor sit amet, consectetur adipisicing elit';
+      var res = rlp.encode(data);
+      res.should.equal(exp);
+    })
+  })
+
   describe('#decode', function(){
     it('should decode cat', function(){
       var data = '\x83cat';
@@ -11,5 +62,6 @@ describe('rlp', function(){
       var res = rlp.decode(data);
       res.should.equal(exp);
     })
+  })
 })
 
