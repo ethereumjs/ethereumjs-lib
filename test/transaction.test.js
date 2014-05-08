6be04838fdf2e1673bdecff8cef2ab81f948fb2e
ethers <ethereum@outlook.com>
2014-05-08 15:20:50 -0700

    Make transactions with data
    Already seems to be working and test added for it.
    The test is the hypothetical transaction that will be sent to
    the namecoin contract to register [george,45], thus note nonce is 1.

--- test/transaction.test.js
+++ test/transaction.test.js
@@ -4,19 +4,29 @@ var transaction = require('../src/transaction');
 var util = require('../src/util');
 
 describe('transaction', function(){
+  describe('#mktx', function(){
+    it('should make transaction with data [george,45]', function(){
+      var contractAddr = 'da7ce79725418f4f6e13bf5f520c89cec5f6a974';
+      var data = '000000000000000000000000000000000000000000000000000067656f726765000000000000000000000000000000000000000000000000000000000000002d';
+      var exp = 'f8640185e8d4a5100082271094da7ce79725418f4f6e13bf5f520c89cec5f6a974822710b840000000000000000000000000000000000000000000000000000067656f726765000000000000000000000000000000000000000000000000000000000000002d';
+      var res = transaction.mktx(util.bigInt(1), contractAddr, util.bigInt(10000), data);
+      res.should.equal(exp);
+    });     
+  });
+  
   describe('#mkContract', function(){
     it('should make namecoin', function(){
       var code = '600035566300000021596020356000355760015b525b54602052f2630000002b5860005b525b54602052f2';
       var exp = 'f8388085e8d4a510008227108080ab600035566300000021596020356000355760015b525b54602052f2630000002b5860005b525b54602052f2';
       var res = transaction.mkContract(util.bigInt(0), util.bigInt(0), code);
       res.should.equal(exp);
-    })
-  })
+    }); 
+  });
 
   describe('#sign', function(){
     it('should sign a data-less transaction', function(){
       var to = 'cd2a3d9f938e13cd947ec05abc7fe734df8dd826'; // cow
-      var key = '0c06818f82e04c564290b32ab86b25676731fc34e9a546108bf109194c8e3aae' // cow1
+      var key = '0c06818f82e04c564290b32ab86b25676731fc34e9a546108bf109194c8e3aae'; // cow1
       var expTx = 'e18085e8d4a5100082271094cd2a3d9f938e13cd947ec05abc7fe734df8dd8260d80';
       var exp = 'f8648085e8d4a5100082271094cd2a3d9f938e13cd947ec05abc7fe734df8dd8260d801ca0f9ee5ff3ef4c0cab613471732a3ecc0b27181504ddaeae6f0a9892ee4e03c10ba01f5fbfcea3110c93eebf3fd4ccb49c27890f7899b3d7ee3c15fa8ac4c4eb39ab';
 
@@ -24,7 +34,7 @@ describe('transaction', function(){
       tx.should.equal(expTx);
       var parsedTx = transaction.parse(util.decodeHex(tx));
       transaction.sign(parsedTx, key).should.equal(exp);
-    })
+    });
 
     it('should sign namecoin contract', function(){
       var namecoin = '600035566300000021596020356000355760015b525b54602052f2630000002b5860005b525b54602052f2';
@@ -40,7 +50,7 @@ describe('transaction', function(){
       parsedTx.data.should.eql(util.decodeHex(namecoin));
 
       transaction.sign(parsedTx, key).should.equal(exp);
-    })
-  })
-})
+    });
+  });
+});
 
