16efe721d2c788f48f473073512c227e893e7e3e
ethers <ethereum@outlook.com>
2014-05-17 04:17:53 -0700

    fix trie usage of sha3, add rootHash() and tests, and jshint fixes
    some tests from pyethereum features/trie.feature

--- src/trie.js
+++ src/trie.js
@@ -1,124 +1,154 @@
 var convert = require('./convert'),
     util = require('./util'),
-    rlp = require('./rlp')
+    rlp = require('./rlp');
+
+var BLANK = '';
+var BLANK_ROOT = util.repeat('\x00', 32);
 
 var BasicDB = function() {
-    this.map = {}
-    this.get = function(k) { return this.map[k] }
-    this.set = function(k,v) { this.map[k] = v }
-    return this
-}
+    this.map = {};
+    this.get = function(k) {
+        return this.map[k];
+    };
+    this.set = function(k, v) {
+        this.map[k] = v;
+    };
+    return this;
+};
 
-var Trie = function(db,node) {
-    this.root = node || ''
-    this.db = db || new BasicDB()
-}
+var Trie = function(db, node) {
+    this.root = node || BLANK;
+    this.db = db || new BasicDB();
+};
 
 Trie.prototype._dbget = function(v) {
-    if (!v) return ''
-    if (!util.isString(v) || v.length < 32) return v.slice()
+    if (!v) return BLANK;
+    if (!util.isString(v) || v.length < 32) return v.slice();
     var n = this.db.get(v);
-    return n ? rlp.decode(n) : null
-}
+    return n ? rlp.decode(n) : null;
+};
 
 Trie.prototype._dbset = function(v) {
-    if (!v) return ''
-    rlp_node = rlp.encode(v)
-    if (rlp_node.length < 32) return v.slice(0)
-    var h = convert.bytesToString(convert.hexToBytes(util.sha3(rlp_node)))
-    this.db.set(h, rlp_node)
-    return h
-}
+    if (!v) return BLANK;
+    var rlp_node = rlp.encode(v);
+    if (rlp_node.length < 32) return v.slice(0);
+    var h = util.sha3(rlp_node);
+    this.db.set(h, rlp_node);
+    return h;
+};
 
 Trie.prototype._get = function(node, path) {
-    if (path.length == 0 || !node) {
+    if (path.length === 0 || !node) {
         return node;
     }
-    node = this._dbget(node)
+    node = this._dbget(node);
     if (node.length == 17) {
-        var subnode = this._dbget(node[path[0]])
-        return this._get(subnode, path.slice(1))
+        var subnode = this._dbget(node[path[0]]);
+        return this._get(subnode, path.slice(1));
     }
     else if (node.length == 2) {
-        var oldkey = compactDecode(node[0])
-        var shared = 0
-        while (shared < oldkey.length && shared < path.length && 
-                oldkey[shared] === path[shared]) shared++;
-        if (shared < oldkey.length) return ''
-        else return this._get(node[1], path.slice(shared))
+        var oldkey = compactDecode(node[0]);
+        var shared = 0;
+        while (shared < oldkey.length && shared < path.length && oldkey[shared] === path[shared]) {
+            shared++;
+        }
+        if (shared < oldkey.length) return BLANK;
+        else return this._get(node[1], path.slice(shared));
     }
-}
+};
 
 Trie.prototype._update = function(node, path, value) {
-    if (path.length == 0) {
+    var sub_node;
+
+    if (path.length === 0) {
         return value;
     }
-    node = this._dbget(node)
+    node = this._dbget(node);
     //console.log(1, node)
-    if (node.length == 17) {
-        node[path[0]] = this._update(node[path[0]], path.slice(1), value)
+    if (node.length === 17) {
+        node[path[0]] = this._update(node[path[0]], path.slice(1), value);
     }
-    else if (node.length == 0) {
-        node = [compactEncode(path), value]
+    else if (node.length === 0) {
+        node =[compactEncode(path), value];
     }
     else {
-        var oldkey = compactDecode(node[0])
-        var shared = 0
-        while (shared < oldkey.length && shared < path.length && 
-                oldkey[shared] === path[shared]) shared++;
+        var oldkey = compactDecode(node[0]);
+        var shared = 0;
+        while (shared < oldkey.length && shared < path.length && oldkey[shared] === path[shared]) {
+            shared++;
+        }
         //console.log(1.5,shared,oldkey,path)
         if (!shared) {
-            new_node = ['','','','','','','','','','','','','','','','','']
-            new_node[oldkey[0]] = this._update(new_node[oldkey[0]], oldkey.slice(1), node[1])
-            new_node[path[0]] = this._update(new_node[path[0]], path.slice(1), value)
-            node = new_node
+            var new_node = ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''];
+            new_node[oldkey[0]] = this._update(new_node[oldkey[0]], oldkey.slice(1), node[1]);
+            new_node[path[0]] = this._update(new_node[path[0]], path.slice(1), value);
+            node = new_node;
         }
         else {
-            sub_node = this._update('', oldkey.slice(shared), node[1])
-            sub_node = this._update(sub_node, path.slice(shared), value)
-            node = [compactEncode(oldkey.slice(0,shared)), sub_node]
+            sub_node = this._update(BLANK, oldkey.slice(shared), node[1]);
+            sub_node = this._update(sub_node, path.slice(shared), value);
+            node =[compactEncode(oldkey.slice(0, shared)), sub_node];
         }
     }
     //console.log(2,node)
     // Normalize
-    if (node.length == 17) {
-        var posOfSingle = -1
-        for (var i = 0; i < 17; i++) {
-            if (node[i]) {   
-                if (posOfSingle == -1) posOfSingle = i
-                else if (posOfSingle > 0) posOfSingle = -2
+    if (node.length === 17) {
+        var posOfSingle = -1;
+        for (var i = 0;i < 17;i++) {
+            if (node[i]) {
+                if (posOfSingle === -1) posOfSingle = i;
+                else if (posOfSingle > 0) posOfSingle = -2;
             }
         }
-        if (posOfSingle == -1) return ''
+        if (posOfSingle === -1) return BLANK;
         else if (posOfSingle > 0) {
-            node = [ compactEncode([posOfSingle]), node[posOfSingle] ]
+            node =[compactEncode([posOfSingle]), node[posOfSingle]];
         }
     }
-    if (node.length == 2) {
-        if (node[1] == '') return ''
-        sub_node = this._dbget(node[1])
-        if (sub_node.length == 2) {
-            new_path = compactDecode(node[0]).concat(compactDecode(sub_node[0]))
-            node = [compactEncode(new_path), sub_node[1]]
+    if (node.length === 2) {
+        if (node[1] === BLANK) return BLANK;
+        sub_node = this._dbget(node[1]);
+        if (sub_node && sub_node.length == 2) {
+            new_path = compactDecode(node[0]).concat(compactDecode(sub_node[0]));
+            node =[compactEncode(new_path), sub_node[1]];
         }
     }
     //console.log(3,node)
-    // Database-ize
-    return this._dbset(node)
-}
+       // Database-ize
+    return this._dbset(node);
+};
+
+Trie.prototype._rlp_decode = function(node) {
+    if (!util.isString(node)) {
+        return node;
+    } else if (node.length === 0) {
+        return node;
+    } else if (node.length < 32) {
+        return node;
+    } else {
+        return rlp.decode(this.db.get(node));
+    }
+};
 
 Trie.prototype.get = function(k) {
-   key = compactHexDecode(k).concat([16])
-   return this._get(this.root,key)
-}
+    var key = compactHexDecode(k).concat([16]);
+    return this._get(this.root, key);
+};
 
 Trie.prototype.update = function(k,v) {
-   key = compactHexDecode(k).concat([16])
-   this.root = this._update(this.root,key,v)
-}
+    var key = compactHexDecode(k).concat([16]);
+    this.root = this._update(this.root, key, v);
+};
+
+// based on pyethereum features/steps/trie.py
+Trie.prototype.rootHash = function() {
+    var rlpRoot = rlp.encode(this._rlp_decode(this.root));
+    return util.encodeHex(util.sha3(rlpRoot));
+};
+
 
 var compactEncode = function (dataArr) {
-  dataArr = dataArr.slice()
+  dataArr = dataArr.slice();
   if (util.isArray(dataArr)) {
     var terminator = 0;
     if (dataArr[dataArr.length - 1] === 16) {
@@ -136,9 +166,9 @@ var compactEncode = function (dataArr) {
     } else {
       dataArr.unshift(flags, 0);
     }
-    var o = '';
+    var o = BLANK;
     for (var i = 0; i < dataArr.length; i += 2) {
-      o += String.fromCharCode(16 * dataArr[i] + dataArr[i + 1])
+      o += String.fromCharCode(16 * dataArr[i] + dataArr[i + 1]);
     }
     return o;
   } else {
@@ -153,14 +183,14 @@ var compactEncode = function (dataArr) {
  * @param {String|Buffer} str
  * @returns {Array}
  **/
+
 var compactHexDecode = function (str) {
-  var base = "0123456789abcdef",
-    hexArr = [];
+  var base = "0123456789abcdef";
+  var hexArr =[];
   str = convert.bytesToHex(convert.stringToBytes(str));
   for (var i = 0; i < str.length; i++) {
     hexArr.push(base.indexOf(str[i]));
   }
-
   return hexArr;
 };
 
@@ -170,9 +200,9 @@ var compactHexDecode = function (str) {
  * @param {String|Buffer} str
  * @returns {Array}
  **/
+
 var compactDecode = function (str) {
   var base = compactHexDecode(str);
-
   if (parseInt(base[0], 10) >= 2) {
     base.push(16);
   }
@@ -181,13 +211,14 @@ var compactDecode = function (str) {
   } else {
     base = base.slice(2);
   }
-
   return base;
 };
 
 module.exports = {
     Trie: Trie,
+    BLANK: BLANK,
+    BLANK_ROOT: BLANK_ROOT,
     BasicDB: BasicDB,
     compactDecode: compactDecode,
     compactEncode: compactEncode
-}
+};
---
+++ test/trie.test.js
@@ -0,0 +1,88 @@
+require('chai').should();
+
+var Trie = require('../src/trie');
+
+describe('trie', function(){
+
+  var LONG_VALUE = "1234567890abcdefghijklmnopqrstuvwxxzABCEFGHIJKLMNOPQRSTUVWXYZ";
+
+  describe('#update', function() {
+    it('should get updated value', function () {
+      var trie = new Trie.Trie(undefined, '');
+      trie.update('dog', LONG_VALUE);
+      var result = trie.get('dog');
+      result.should.equal(LONG_VALUE);
+    });
+
+    it('should do replace when update with same key', function () {
+      var trie = new Trie.Trie(undefined, '');
+      trie.update('dog', LONG_VALUE);
+      trie.update('dog', LONG_VALUE + 'sameKey');
+      var result = trie.get('dog');
+      result.should.equal(LONG_VALUE + 'sameKey');
+    });
+
+    it('basic', function () {
+      var trie = new Trie.Trie(undefined, '');
+      trie.update('a', 'A');
+      trie.update('b', 'B');
+      trie.get('a').should.equal('A');
+      trie.get('b').should.equal('B');
+    });
+  });
+
+  describe('#rootHash', function (argument) {
+    it('basic', function () {
+      var trie = new Trie.Trie(undefined, '');
+      trie.update('a', 'A');
+      trie.update('b', 'B');
+      trie.rootHash().should.equal('300eab197a9d9e437aaeb9b0d7bd77d57e8d4e3eeca0b1e6a3fe28a84e2cd70c');
+    });
+
+    it('basic1', function () {
+      var trie = new Trie.Trie(undefined, '');
+      trie.update('test', 'test');
+      trie.rootHash().should.equal('85d106d4edff3b7a4889e91251d0a87d7c17a1dda648ebdba8c6060825be23b8');
+    });
+
+    it('basic2', function () {
+      var trie = new Trie.Trie(undefined, '');
+      trie.update('test', 'test');
+      trie.update('te', 'testy');
+      trie.rootHash().should.equal('8452568af70d8d140f58d941338542f645fcca50094b20f3c3d8c3df49337928');
+    });
+
+    it('do prefix', function () {
+      var trie = new Trie.Trie(undefined, '');
+      trie.update('dogglesworth', 'cat');
+      trie.update('dog', 'puppy');
+      trie.update('doe', 'reindeer');
+      trie.rootHash().should.equal('8aad789dff2f538bca5d8ea56e8abe10f4c7ba3a5dea95fea4cd6e7c3a1168d3');
+    });
+
+    it('be prefix', function () {
+      var trie = new Trie.Trie(undefined, '');
+      trie.update('be', 'e');
+      trie.update('dog', 'puppy');
+      trie.update('bed', 'd');
+      trie.rootHash().should.equal('3f67c7a47520f79faa29255d2d3c084a7a6df0453116ed7232ff10277a8be68b');
+    });
+
+    it('multiprefix', function () {
+      var trie = new Trie.Trie(undefined, '');
+      trie.update('dog', 'puppy');
+      trie.update('horse', 'stallion');
+      trie.update('do', 'verb');
+      trie.update('doge','coin');
+      trie.rootHash().should.eql('5991bb8c6514148a29db676a14ac506cd2cd5775ace63c30a4fe457715e9ac84');
+    });
+
+    it('replacement', function () {
+      var trie = new Trie.Trie(undefined, '');
+      trie.update('foo', 'bar');
+      trie.update('food', 'bat');
+      trie.update('food', 'bass');
+      trie.rootHash().should.equal('17beaa1648bafa633cda809c90c04af50fc8aed3cb40d16efbddee6fdf63c4c3');
+    });
+  });
+});
