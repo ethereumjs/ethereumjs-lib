288bd358dcca06062220a5e7adfce4dac1e28464
ethers <ethereum@outlook.com>
2014-05-22 00:47:33 -0700

    use latest genesis block

--- test/block.test.js
+++ test/block.test.js
@@ -2,12 +2,13 @@ require('chai').should();
 
 var block = require('../src/block');
 var BigInteger = require('../src/jsbn/jsbn');
+var genesisData = require('./jsonData/genesishashestest.json');
 
 describe('block', function(){
   describe('genesis', function(){
     it('should have correct state root', function(){
       var b = block.genesis();
-      b.stateRoot().should.eql('2f4399b08efe68945c1cf90ffe85bbe3ce978959da753f9e649f034015b8817d');
+      b.stateRoot().should.eql(genesisData.genesis_state_root);
     });
   });
 
