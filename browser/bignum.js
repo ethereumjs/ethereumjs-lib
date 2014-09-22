fc90172161a11cf4654dd80d873ece796192bc97
wanderer <mjbecze@gmail.com>
2014-09-22 13:17:43 -0400

    passing initail for VM in browser

--- browser/bignum.js
+++ browser/bignum.js
@@ -1,14 +1,48 @@
 var bigi = require('bigi');
 
-bigi.prototype.mul = bigi.prototype.multiply;
-bigi.prototype.eq = bigi.prototype.equals;
-
-module.exports = function(i, r){
-  if('number' == typeof i){
-    i = i.toString(); 
+var bi = function (i, r) {
+  if ('number' == typeof i) {
+    i = i.toString();
+  } else if (bigi.isBigInteger(i)) {
+    return i;
   }
 
   return bigi(i, r);
 };
 
+bi.fromBuffer = function (buf) {
+  return bigi.fromBuffer(buf);
+};
 
+bigi.prototype.mul = wrap('multiply');
+bigi.prototype.eq = wrap('equals');
+bigi.prototype.sub = wrap('subtract');
+
+bigi.prototype.toNumber = function () {
+  return Number(this.toString());
+};
+
+bigi.prototype.ge = function (n) {
+  var c = this.compareTo(n);
+  if (c >= 0) {
+    return true;
+  }
+  return false;
+};
+
+bigi.prototype.lt = function (n) {
+  var c = this.compareTo(n);
+  if (c < 0) {
+    return true;
+  }
+  return false;
+};
+
+
+module.exports = bi;
+
+function wrap(f2) {
+  return function (i) {
+    return this[f2](bi(i));
+  };
+}
