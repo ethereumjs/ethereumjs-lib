6c6a9fc72a2acd7fb5704004168774c283544d0e
ethers <ethereum@outlook.com>
2014-10-10 13:56:17 -0700

    test key and addr

--- test/common/keyddr.js
+++ test/common/keyddr.js
@@ -1,5 +1,19 @@
-var tests = require('../../../tests/keyaddrtest.json');
+var tests = require('../../../tests/keyaddrtest.json'),
+  assert = require('assert'),
+  utils = require('../../lib/utils'),
+  ecdsa = require('secp256k1'),
+  SHA3 = require('sha3');
 
-describe('keyaddr tests', function () {
-  console.log(tests)
+it('keyaddr tests', function () {
+  tests.forEach(function(data) {
+    var hash = new SHA3.SHA3Hash(256);
+    hash.update(data.seed);
+
+    var privKey = hash.digest('hex');
+    assert(privKey === data.key);
+
+    var pubKey = ecdsa.createPublicKey(new Buffer(privKey, 'hex'));
+    var addr = utils.pubToAddress(pubKey).toString('hex');
+    assert(addr === data.addr)
+  });
 });
