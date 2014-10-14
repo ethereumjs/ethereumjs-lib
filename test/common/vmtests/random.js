e56cf63acf832b08e7f098eaebc69bab99a333e7
ethers <ethereum@outlook.com>
2014-10-14 11:55:48 -0700

    set up env

--- test/common/vmtests/random.js
+++ test/common/vmtests/random.js
@@ -3,6 +3,7 @@ var testData = require('../../../../tests/vmtests/random.json'),
   rlp = require('rlp'),
   VM = require('../../../lib/vm'),
   Account = require('../../../lib/account.js'),
+  Block = require('../../../lib/block.js'),
   utils = require('../../../lib/utils.js'),
   Tx = require('../../../lib/transaction.js'),
   assert = require('assert'),
@@ -17,9 +18,31 @@ var internals = {},
 internals.state = new Trie(stateDB);
 
 describe('[VM]: Basic functions', function () {
+  testData = testData.random;
+
   it('setup the trie', function (done) {
-      // var test = vmTests.txTest;
-      // var account = new Account(test.preFromAccount);
+    var pre,
+      account;
+
+    // todo async
+    testdata.pre.forEach(function(acctData) {
+      pre = [
+        acctData.nonce,
+        acctData.balance,
+        // stateRoot?
+        // codeHash?
+      ];
+
+      account = new Account(pre);
       // internals.state.put(new Buffer(test.from, 'hex'), account.serialize(), done);
+    })
+
+    var block = new Block();
+    block.header.timestamp = testData.currentTimestamp;
+    block.header.gasLimit = testData.currentGasLimit;
+    block.header.parentHash = testData.previousHash;
+    block.header.coinbase = testData.currentCoinbase;
+    block.header.difficulty = testData.currentDifficulty;
+    block.header.number = testData.currentNumber;
   });
 });
