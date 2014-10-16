0b855ed0768c5a418ae4e04a0a7d6dec54517abc
ethers <ethereum@outlook.com>
2014-10-15 17:14:03 -0700

    add difficulty test

--- test/common/vmtests/vmBlockInfo.js
+++ test/common/vmtests/vmBlockInfo.js
@@ -1,4 +1,4 @@
-var testData = require('../../../../tests/vmtests/vmBlockInfoTest.json'),
+var vmBlockInfoTest = require('../../../../tests/vmtests/vmBlockInfoTest.json'),
   async = require('async'),
   VM = require('../../../lib/vm'),
   Account = require('../../../lib/account.js'),
@@ -16,7 +16,7 @@ var stateDB = levelup('', {
 describe('[Common]: vmBlockInfoTest', function () {
 
   describe('coinbase', function () {
-    testData = testData.coinbase;
+    var testData = vmBlockInfoTest.coinbase;
 
     it('setup the trie', function (done) {
       var keysOfPre = Object.keys(testData.pre),
@@ -73,4 +73,64 @@ describe('[Common]: vmBlockInfoTest', function () {
       });
     });
   });
+
+  describe('difficulty', function () {
+    var testData = vmBlockInfoTest.difficulty;
+
+    it('setup the trie', function (done) {
+      var keysOfPre = Object.keys(testData.pre),
+        acctData,
+        account;
+
+      async.each(keysOfPre, function(key, callback) {
+        acctData = testData.pre[key];
+
+        account = new Account();
+        account.nonce = testUtils.fromDecimal(acctData.nonce);
+        account.balance = testUtils.fromDecimal(acctData.balance);
+        state.put(new Buffer(key, 'hex'), account.serialize(), callback);
+      }, done);
+    });
+
+    it('run code', function(done) {
+      var env = testData.env,
+        block = testUtils.makeBlockFromEnv(env),
+        acctData,
+        account,
+        runCodeData,
+        vm = new VM(state);
+
+      acctData = testData.pre[testData.exec.address];
+      account = new Account();
+      account.nonce = testUtils.fromDecimal(acctData.nonce);
+      account.balance = testUtils.fromDecimal(acctData.balance);
+
+      runCodeData = testUtils.makeRunCodeData(testData.exec, account, block);
+      vm.runCode(runCodeData, function(err, results) {
+        assert(!err);
+        assert(results.gasUsed.toNumber() === (testData.exec.gas - testData.gas));
+
+        var keysOfPost = Object.keys(testData.post);
+        async.each(keysOfPost, function(key, callback) {
+          acctData = testData.post[key];
+
+          var account = results.account;
+          assert(testUtils.toDecimal(account.balance) === acctData.balance);
+          assert(testUtils.toDecimal(account.nonce) === acctData.nonce);
+
+          state.root = account.stateRoot.toString('hex');
+
+          var storageKeys = Object.keys(acctData.storage);
+          storageKeys.forEach(function(skey) {
+            state.get(testUtils.address(skey), function(err, data) {
+              assert(!err);
+              assert(rlp.decode(data).toString('hex') === acctData.storage[skey].slice(2));
+              callback();
+            });
+          });
+        }, done);
+      });
+    });
+  });
+
 });
