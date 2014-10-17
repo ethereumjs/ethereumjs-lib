dc157c51a2ae36180f9c0eeafc960e576b69f7de
ethers <ethereum@outlook.com>
2014-10-17 16:20:08 -0700

    use verifyAccountPostConditions

--- test/common/vmtests/vmBlockInfo.js
+++ test/common/vmtests/vmBlockInfo.js
@@ -4,7 +4,6 @@ var vmBlockInfoTest = require('../../../../tests/vmtests/vmBlockInfoTest.json'),
   Account = require('../../../lib/account.js'),
   assert = require('assert'),
   testUtils = require('../../testUtils'),
-  rlp = require('rlp'),
   Trie = require('merkle-patricia-tree');
 
 describe('[Common]: vmBlockInfoTest', function () {
@@ -35,41 +34,28 @@ describe('[Common]: vmBlockInfoTest', function () {
         assert(!err);
         assert(results.gasUsed.toNumber() === (testData.exec.gas - testData.gas));
 
-        // validate the postcondition of account
-        acctData = testData.post[testData.exec.address];
-        account = results.account;
-        assert(testUtils.toDecimal(account.balance) === acctData.balance);
-        assert(testUtils.toDecimal(account.nonce) === acctData.nonce);
+        async.series([
+          function(cb) {
+            account = results.account;
+            acctData = testData.post[testData.exec.address];
+            testUtils.verifyAccountPostConditions(state, account, acctData, cb);
+          },
 
-        // validate the postcondition of other accounts
-        delete testData.post[testData.exec.address];
-        var keysOfPost = Object.keys(testData.post);
-        async.each(keysOfPost, function(key, callback) {
-          acctData = testData.post[key];
+          function() {
+            // validate the postcondition of other accounts
+            delete testData.post[testData.exec.address];
+            var keysOfPost = Object.keys(testData.post);
+            async.each(keysOfPost, function(key, cb) {
+              state.get(new Buffer(key, 'hex'), function(err, raw) {
+                assert(!err);
 
-          state.get(new Buffer(key, 'hex'), function(err, raw) {
-            assert(!err);
-
-            account = new Account(raw);
-            assert(testUtils.toDecimal(account.balance) === acctData.balance);
-            assert(testUtils.toDecimal(account.nonce) === acctData.nonce);
-
-            // validate storage
-            var storageKeys = Object.keys(acctData.storage);
-            if (storageKeys.length > 0) {
-              state.root = account.stateRoot.toString('hex');
-              storageKeys.forEach(function(skey) {
-                state.get(testUtils.address(skey), function(err, data) {
-                  assert(!err);
-                  assert(rlp.decode(data).toString('hex') === acctData.storage[skey].slice(2));
-                  callback();
-                });
+                account = new Account(raw);
+                acctData = testData.post[key];
+                testUtils.verifyAccountPostConditions(state, account, acctData, cb);
               });
-            } else {
-              callback();
-            }
-          });
-        }, done);
+            }, done);
+          }
+        ]);
       });
     });
   });
