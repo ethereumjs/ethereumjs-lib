2c4532b6efb3392b275f2ca19e962552154b3bfe
ethers <ethereum@outlook.com>
2014-10-17 15:17:57 -0700

    will need to check storage

--- test/common/vmtests/vmSha3.js
+++ test/common/vmtests/vmSha3.js
@@ -53,41 +53,51 @@ describe('[Common]: vmSha3', function () {
         assert(!err);
         assert(results.gasUsed.toNumber() === (testData.exec.gas - testData.gas));
 
-        // validate the postcondition of account
-        acctData = testData.post[testData.exec.address];
-        account = results.account;
-        assert(testUtils.toDecimal(account.balance) === acctData.balance);
-        assert(testUtils.toDecimal(account.nonce) === acctData.nonce);
-
-        // validate the postcondition of other accounts
-        delete testData.post[testData.exec.address];
-        var keysOfPost = Object.keys(testData.post);
-        async.each(keysOfPost, function(key, callback) {
-          acctData = testData.post[key];
-
-          state.get(new Buffer(key, 'hex'), function(err, raw) {
-            assert(!err);
-
-            account = new Account(raw);
+        async.series([
+          function(cb) {
+            // validate the postcondition of account
+            acctData = testData.post[testData.exec.address];
+            account = results.account;
             assert(testUtils.toDecimal(account.balance) === acctData.balance);
             assert(testUtils.toDecimal(account.nonce) === acctData.nonce);
 
-            // validate storage
-            var storageKeys = Object.keys(acctData.storage);
-            if (storageKeys.length > 0) {
-              state.root = account.stateRoot.toString('hex');
-              storageKeys.forEach(function(skey) {
-                state.get(testUtils.address(skey), function(err, data) {
-                  assert(!err);
-                  assert(rlp.decode(data).toString('hex') === acctData.storage[skey].slice(2));
+            // todo: check storage
+            cb();
+          },
+
+          function(cb) {
+            // validate the postcondition of other accounts
+            delete testData.post[testData.exec.address];
+            var keysOfPost = Object.keys(testData.post);
+            console.log('kpost: ', keysOfPost)
+            async.each(keysOfPost, function(key, callback) {
+              acctData = testData.post[key];
+
+              state.get(new Buffer(key, 'hex'), function(err, raw) {
+                assert(!err);
+
+                account = new Account(raw);
+                assert(testUtils.toDecimal(account.balance) === acctData.balance);
+                assert(testUtils.toDecimal(account.nonce) === acctData.nonce);
+
+                // validate storage
+                var storageKeys = Object.keys(acctData.storage);
+                if (storageKeys.length > 0) {
+                  state.root = account.stateRoot.toString('hex');
+                  storageKeys.forEach(function(skey) {
+                    state.get(testUtils.address(skey), function(err, data) {
+                      assert(!err);
+                      assert(rlp.decode(data).toString('hex') === acctData.storage[skey].slice(2));
+                      callback();
+                    });
+                  });
+                } else {
                   callback();
-                });
+                }
               });
-            } else {
-              callback();
-            }
-          });
-        }, done);
+            }, done);
+          }
+        ]);
       });
     });
   });
