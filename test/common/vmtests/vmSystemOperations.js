7ccb3aebb7bd6103f08e06e686ae7bb2603db55d
ethers <ethereum@outlook.com>
2014-10-20 18:12:28 -0700

    adding sysem operations test

---
+++ test/common/vmtests/vmSystemOperations.js
@@ -0,0 +1,65 @@
+var vmSystemOperationsTest = require('../../../../tests/vmtests/vmSystemOperationsTest.json'),
+  async = require('async'),
+  VM = require('../../../lib/vm'),
+  Account = require('../../../lib/account.js'),
+  assert = require('assert'),
+  testUtils = require('../../testUtils'),
+  Trie = require('merkle-patricia-tree');
+
+describe('[Common]: vmSystemOperationsTest', function () {
+  var tests = Object.keys(vmSystemOperationsTest);
+  tests = ['ABAcalls0']
+  tests.forEach(function(testKey) {
+    var state = new Trie();
+    var testData = vmSystemOperationsTest[testKey];
+
+    it(testKey + ' setup the trie', function (done) {
+      testUtils.setupPreConditions(state, testData, done);
+    });
+
+    it(testKey + ' run code', function(done) {
+      var env = testData.env,
+        block = testUtils.makeBlockFromEnv(env),
+        acctData,
+        account,
+        runCodeData,
+        vm = new VM(state);
+
+      acctData = testData.pre[testData.exec.address];
+      account = new Account();
+      account.nonce = testUtils.fromDecimal(acctData.nonce);
+      account.balance = testUtils.fromDecimal(acctData.balance);
+
+      runCodeData = testUtils.makeRunCodeData(testData.exec, account, block);
+      vm.runCode(runCodeData, function(err, results) {
+        assert(!err);
+        assert.strictEqual(results.gasUsed.toNumber(),
+          testData.exec.gas - testData.gas, 'gas used mismatch');
+
+        async.series([
+          function(cb) {
+            account = results.account;
+            acctData = testData.post[testData.exec.address];
+            testUtils.verifyAccountPostConditions(state, account, acctData, cb);
+          },
+
+          function() {
+            // validate the postcondition of other accounts
+            delete testData.post[testData.exec.address];
+            var keysOfPost = Object.keys(testData.post);
+            async.each(keysOfPost, function(key, cb) {
+              state.get(new Buffer(key, 'hex'), function(err, raw) {
+                assert(!err);
+
+                account = new Account(raw);
+                acctData = testData.post[key];
+                testUtils.verifyAccountPostConditions(state, account, acctData, cb);
+              });
+            }, done);
+          }
+        ]);
+      });
+    });
+  });
+
+});
