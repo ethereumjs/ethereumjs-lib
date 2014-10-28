c6a8fc0b6a1c714f3cc3f79629bfbfe0518b08bc
wanderer <mjbecze@gmail.com>
2014-10-28 15:01:57 -0400

    added CALLCODE

--- test/common/vmtests/vmSystemOperations.js
+++ test/common/vmtests/vmSystemOperations.js
@@ -200,25 +200,25 @@ describe('[Common]: vmSystemOperationsTest', function() {
         vm = new VM(state);
 
 
-      vm.onStep = function(info, done) {
-        console.log('vm', bignum(info.pc).toString(16) + ' Opcode: ' + info.opcode + ' Gas: ' + info.gasLeft.toString());
+      // vm.onStep = function(info, done) {
+      //   console.log('vm', bignum(info.pc).toString(16) + ' Opcode: ' + info.opcode + ' Gas: ' + info.gasLeft.toString());
 
-        // var stream = vm.trie.createReadStream();
-        // stream.on("data", function(data) {
-        //   var account = new Account(data.value);
-        //   console.log("key: " + data.key.toString("hex"));
-        //   //console.log(data.value.toString('hex'));
-        //   console.log('decoded:' + bignum.fromBuffer(account.balance).toString() + '\n');
-        // });
-        // stream.on('end', done);
+      //   // var stream = vm.trie.createReadStream();
+      //   // stream.on("data", function(data) {
+      //   //   var account = new Account(data.value);
+      //   //   console.log("key: " + data.key.toString("hex"));
+      //   //   //console.log(data.value.toString('hex'));
+      //   //   console.log('decoded:' + bignum.fromBuffer(account.balance).toString() + '\n');
+      //   // });
+      //   // stream.on('end', done);
 
-        info.stack.reverse();
-        info.stack.forEach(function (item) {
-          console.log('vm', '    ' + item.toString('hex'));
-        });
-        info.stack.reverse();
-        done();
-      };
+      //   info.stack.reverse();
+      //   info.stack.forEach(function (item) {
+      //     console.log('vm', '    ' + item.toString('hex'));
+      //   });
+      //   info.stack.reverse();
+      //   done();
+      // };
 
       acctData = testData.pre[testData.exec.address];
       account = new Account();
