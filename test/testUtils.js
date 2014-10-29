d72498271220452bc8d3ff35b062684800bdf1c5
ethers <ethereum@outlook.com>
2014-10-28 17:18:16 -0700

    storage needs to be checked

--- test/testUtils.js
+++ test/testUtils.js
@@ -24,7 +24,7 @@ exports.verifyAccountPostConditions = function(state, account, acctData, cb) {
 
   // validate storage
   var storageKeys = Object.keys(acctData.storage);
-  if (storageKeys.length > 0 && false) {
+  if (storageKeys.length > 0) {
     state.root = account.stateRoot.toString('hex');
     storageKeys.forEach(function(skey) {
       state.get(testUtils.fromAddress(skey), function(err, data) {
