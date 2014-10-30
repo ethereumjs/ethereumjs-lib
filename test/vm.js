879a155c12059ff85355d89fe6de6c905847c640
ethers <ethereum@outlook.com>
2014-10-30 16:08:08 -0700

    makeRunCodeData

--- test/vm.js
+++ test/vm.js
@@ -112,6 +112,17 @@ describe('[VM]: Basic functions', function () {
     "previousHash" : "5e20a0453cecd065ea59c37ac63e079ee08998b6045136a8ce6635c7912ec0b6"
   };
 
+  var exec = {
+    "address" : "0f572e5295c57f15886f9b263e2f6d2d6c7b5ec6",
+    "caller" : "cd1722f3947def4cf144679da39c4c32bdc35681",
+    "code" : "0x7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff6000547faaffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffaa6020546002600060406000601773945304eb96065b2a98b57a48a06ae28d285a71b5620f4240f1600057",
+    "data" : "0x",
+    "gas" : "10000000000000",
+    "gasPrice" : "100000000000000",
+    "origin" : "cd1722f3947def4cf144679da39c4c32bdc35681",
+    "value" : "100000"
+  };
+
   it('t256sha', function (done) {
     stateDB = levelup('', {
       db: require('memdown')
@@ -123,11 +134,6 @@ describe('[VM]: Basic functions', function () {
 
     var block = testUtils.makeBlockFromEnv(env);
 
-
-
-    // makeRunCodeData = function(exec, account, block)
-
-
     var theCode = '0x60016000546020600060206000601360026009f1';
 
     var account = new Account();
@@ -138,18 +144,9 @@ describe('[VM]: Basic functions', function () {
     var expSha256Of1 = '6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b';
     var expSha256Of32bitsWith1 = 'c386d8e8d07342f2e39e189c8e6c57bb205bb373fe4e3a6f69404a8bb767b417';
 
-    runCodeData = {
-      account: account,
-      origin: new Buffer('cd1722f3947def4cf144679da39c4c32bdc35681', 'hex'),
-      code: new Buffer(theCode.slice(2), 'hex'), // slice off 0x
-      value: testUtils.fromDecimal('13'),
-      address: new Buffer('0f572e5295c57f15886f9b263e2f6d2d6c7b5ec6', 'hex'),
-      from: new Buffer('cd1722f3947def4cf144679da39c4c32bdc35681', 'hex'),
-      data: new Buffer('0x'.slice(2), 'hex'), // slice off 0x
-      gasLimit: '10000000000000',
-      gasPrice: testUtils.fromDecimal('100000000000000'),
-      block: block
-    };
+    var runCodeData = testUtils.makeRunCodeData(exec, account, block);
+    runCodeData.code = new Buffer(theCode.slice(2), 'hex'); // slice off 0x
+
     vm.runCode(runCodeData, function(err, results) {
       // TODO
       done();
