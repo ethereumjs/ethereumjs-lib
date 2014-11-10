1b3224bd3f1d3944386a8f1963dc58e0b015a928
ethers <TODO>
2014-11-10 13:45:56 -0800

    starting sys ops tests

---
+++ test/common/StateTests/stSystemOperations.js
@@ -0,0 +1,48 @@
+var stSystemOperationsTest = require('ethereum-tests').StateTests.stSystemOperationsTest,
+  async = require('async'),
+  VM = require('../../../lib/vm'),
+  Account = require('../../../lib/account.js'),
+  assert = require('assert'),
+  testUtils = require('../../testUtils'),
+  Trie = require('merkle-patricia-tree');
+
+describe('[Common]: stSystemOperationsTest', function () {
+  var tests = Object.keys(stSystemOperationsTest);
+
+  tests.forEach(function(testKey) {
+    var state = new Trie();
+    var testData = stSystemOperationsTest[testKey];
+
+    it(testKey + ' setup the trie', function (done) {
+      testUtils.setupPreConditions(state, testData, done);
+    });
+
+    it(testKey + ' run code', function(done) {
+      var env = testData.env,
+        block = testUtils.makeBlockFromEnv(env),
+        acctData,
+        account,
+        vm = new VM(state),
+        tx = testUtils.makeTx(testData.transaction);
+
+      vm.runTx(tx, block, function(err, results) {
+        assert(!err);
+        assert.strictEqual(results.vm.returnValue.toString('hex'), testData.out.slice(2));
+        // TODO assert.strictEqual(results.gasUsed.toNumber(),
+        //   testData.exec.gas - testData.gas, 'gas used mismatch');
+
+        delete testData.post[testData.env.currentCoinbase];  // coinbase is only done in runBlock
+        var keysOfPost = Object.keys(testData.post);
+        async.eachSeries(keysOfPost, function(key, cb) {
+          state.get(new Buffer(key, 'hex'), function(err, raw) {
+            assert(!err);
+
+            account = new Account(raw);
+            acctData = testData.post[key];
+            testUtils.verifyAccountPostConditions(state, account, acctData, cb);
+          });
+        }, done);
+      });
+    });
+  });
+});
