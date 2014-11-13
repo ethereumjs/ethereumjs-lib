b42c04092011779683a38c0cba8520f582f09e9f
ethers <ethereum@outlook.com>
2014-11-12 18:45:38 -0800

    run the fast sysops tests by default

--- test/common/StateTests/stSystemOperations.js
+++ test/common/StateTests/stSystemOperations.js
@@ -14,14 +14,62 @@ function expectError(testKey) {
   return false;
 }
 
-describe('[Common]: stSystemOperationsTest', function () {
+describe('[Common]: stSystemOperationsTest FAST ONLY', function () {
   var tests = Object.keys(stSystemOperationsTest);
   tests.forEach(function(testKey) {
-    // TODO
     if (testKey.match(
-      /^ABAcalls|^CallRecursiveBomb/
+      /^ABAcalls1$|^ABAcalls2$|^ABAcalls3$|^CallRecursiveBomb/
     )) { return; }
 
+    var state = new Trie();
+    var testData = stSystemOperationsTest[testKey];
+
+    it(testKey + ' setup the trie', function (done) {
+      testUtils.setupPreConditions(state, testData, done);
+    });
+
+    it(testKey + ' run code', function(done) {
+      var env = testData.env,
+        block = testUtils.makeBlockFromEnv(env),
+        acctData,
+        account,
+        vm = new VM(state),
+        tx = testUtils.makeTx(testData.transaction);
+
+      // testUtils.enableVMtracing(vm);
+
+      vm.runTx(tx, block, function(err, results) {
+        if (!expectError(testKey)) {
+          assert(!err);
+        }
+
+        if (testData.out.slice(2)) {
+          assert.strictEqual(results.vm.returnValue.toString('hex'), testData.out.slice(2), 'invalid return value');
+        }
+        // TODO assert.strictEqual(results.gasUsed.toNumber(),
+        //   testData.exec.gas - testData.gas, 'gas used mismatch');
+
+        delete testData.post[testData.env.currentCoinbase];  // coinbase is only done in runBlock
+
+        var keysOfPost = Object.keys(testData.post);
+        async.eachSeries(keysOfPost, function(key, cb) {
+          state.get(new Buffer(key, 'hex'), function(err, raw) {
+            assert(!err);
+
+            account = new Account(raw);
+            acctData = testData.post[key];
+            testUtils.verifyAccountPostConditions(state, account, acctData, cb);
+          });
+        }, done);
+      });
+    });
+  });
+});
+
+// these need mocha --timeout 900000
+describe.skip('[Common]: stSystemOperationsTest ALL', function () {
+  var tests = Object.keys(stSystemOperationsTest);
+  tests.forEach(function(testKey) {
     var state = new Trie();
     var testData = stSystemOperationsTest[testKey];
 
