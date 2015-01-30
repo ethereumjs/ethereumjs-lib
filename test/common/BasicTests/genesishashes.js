7562acd417ee42a3e28d089f611e3a203c6f2c4c
wanderer <mjbecze@gmail.com>
2015-01-29 22:38:26 -0500

    changed generating genesis block

--- test/common/BasicTests/genesishashes.js
+++ test/common/BasicTests/genesishashes.js
@@ -2,6 +2,7 @@ var genesisData = require('ethereum-tests').basicTests.genesishashestest,
   assert = require('assert'),
   Blockchain = require('../../../lib/blockchain.js'),
   Block = require('../../../lib/block.js'),
+  VM = require('../../../lib/vm/index.js'),
   levelup = require('levelup');
 
 var blockDB = levelup('', {
@@ -10,12 +11,23 @@ var blockDB = levelup('', {
   detailsDB = levelup('/does/not/matter', {
     db: require('memdown')
   }),
+  stateDB = levelup('/does/not/matter', {
+    db: require('memdown')
+  }),
   blockchain;
 
 describe('[Common]: genesis hashes tests', function () {
   it('should create a new block chain', function (done) {
     blockchain = new Blockchain(blockDB, detailsDB);
     blockchain.init(done);
+  });
+
+  it('should generate the genesis state correctly', function(done){
+    var vm = new VM(stateDB);
+    vm.generateGenesis(genesisData.initial_alloc, function(){
+      assert.equal(vm.trie.root.toString('hex'), genesisData.genesis_state_root);
+      done();
+    });
   });
 
   it('should have added the genesis correctly', function () {
