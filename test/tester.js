46b11b7bca9989b5afbebd9643721963a09f0172
wanderer <mjbecze@gmail.com>
2015-02-10 14:24:10 -0500

    added tester file

---
+++ test/tester.js
@@ -0,0 +1,20 @@
+var ethTests = require('ethereum-tests');
+var test = require('tape');
+var cp = require('child_process');
+
+test('executble test', function(t) {
+  var stateTest = ethTests.stateTests.stRefundTest.RefundOverflow;
+
+  var ejt = cp.spawn(__dirname + '/../bin/tester', ['-r', JSON.stringify(stateTest)]);
+  ejt.stderr.on('data', function(d){
+    t.fail(d.toString());
+  });
+
+  ejt.stdout.on('data', function(data){
+    t.equal(data.toString(), '0', 'should not error' );
+  });
+
+  ejt.on('close', function(){
+    t.end();
+  });
+});
