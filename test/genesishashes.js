d227acbb0aca627f53f869b5f686d9135aeb2f3e
wanderer <mjbecze@gmail.com>
2015-04-07 19:22:42 -0400

    updated blockheader

--- test/genesishashes.js
+++ test/genesishashes.js
@@ -1,28 +1,9 @@
 const genesisData = require('ethereum-tests').basicTests.genesishashestest,
   tape = require('tape'),
-  Blockchain = require('../lib/blockchain.js'),
   Block = require('../lib/block.js'),
-  VM = require('../lib/vm/index.js'),
-  levelup = require('levelup');
-
-var blockDB = levelup('', {
-    db: require('memdown')
-  }),
-  detailsDB = levelup('/does/not/matter', {
-    db: require('memdown')
-  }),
-  stateDB = levelup('/does/not/matter', {
-    db: require('memdown')
-  }),
-  blockchain;
+  VM = require('../lib/vm/index.js');
 
 tape('[Common]: genesis hashes tests', function(t) {
-  t.test('should create a new block chain', function(st) {
-    blockchain = new Blockchain(blockDB, detailsDB);
-    blockchain.init(function() {
-      st.end();
-    });
-  });
 
   // t.test('should generate the genesis state correctly', function(st) {
   //   var vm = new VM(stateDB);
@@ -32,21 +13,17 @@ tape('[Common]: genesis hashes tests', function(t) {
   //   });
   // });
 
-  t.test('should have added the genesis correctly', function(st) {
-    var blockGenesis = new Block(),
-      rlpGenesis;
+  t.test('should generete the genesis correctly', function(st) {
+    var blockGenesis = new Block();
+
     blockGenesis.header.stateRoot = genesisData.genesis_state_root;
 
-    rlpGenesis = blockGenesis.serialize();
-
+    var rlpGenesis = blockGenesis.serialize();
     st.strictEqual(rlpGenesis.toString('hex'),
       genesisData.genesis_rlp_hex, 'rlp hex mismatch');
 
-    blockchain.addBlock(blockGenesis, function() {
-      st.strictEqual(blockchain.meta.genesis,
-        genesisData.genesis_hash, 'genesis hash mismatch');
+    st.strictEqual(blockGenesis.hash().toString('hex'), genesisData.genesis_hash);
+    st.end();
 
-      st.end();
-    });
   });
 });
