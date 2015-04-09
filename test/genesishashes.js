c3a5f2d2928d6f92877fa6784cd68b60d3168961
wanderer <mjbecze@gmail.com>
2015-04-09 16:58:07 -0400

    fixed genesisHash test

--- test/genesishashes.js
+++ test/genesishashes.js
@@ -1,29 +1,29 @@
-const genesisData = require('ethereum-tests').basicTests.genesishashestest,
+const genesisData = require('ethereum-tests').basicTests.genesishashestest;
+const Trie = require('merkle-patricia-tree/secure.js'),
   tape = require('tape'),
   Block = require('../lib/block.js'),
+  common = require('ethereum-common'),
   VM = require('../lib/vm/index.js');
 
-tape('[Common]: genesis hashes tests', function(t) {
+var trie = new Trie();
 
-  // t.test('should generate the genesis state correctly', function(st) {
-  //   var vm = new VM(stateDB);
-  //   vm.generateGenesis(genesisData.initial_alloc, function() {
-  //     st.equal(vm.trie.root.toString('hex'), genesisData.genesis_state_root);
-  //     st.end();
-  //   });
-  // });
+tape('[Common]: genesis hashes tests', function(t) {
+  t.test('should generate the genesis state correctly', function(st) {
+    var vm = new VM(trie);
+    vm.generateGenesis(common.allotments , function() {
+      st.equal(vm.trie.root.toString('hex'), genesisData.genesis_state_root);
+      st.end();
+    });
+  });
 
   t.test('should generete the genesis correctly', function(st) {
     var blockGenesis = new Block();
-
     blockGenesis.header.stateRoot = genesisData.genesis_state_root;
-
     var rlpGenesis = blockGenesis.serialize();
     st.strictEqual(rlpGenesis.toString('hex'),
       genesisData.genesis_rlp_hex, 'rlp hex mismatch');
 
     st.strictEqual(blockGenesis.hash().toString('hex'), genesisData.genesis_hash);
     st.end();
-
   });
 });
