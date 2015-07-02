aeda855626f2714a6aab58a929b3a0dbaac67ca6
wanderer <mjbecze@gmail.com>
2015-07-02 07:56:06 -0400

    add caniconical allotments

--- test/genesishashes.js
+++ test/genesishashes.js
@@ -2,7 +2,6 @@ const genesisData = require('ethereum-tests').basicTests.genesishashestest;
 const Trie = require('merkle-patricia-tree/secure.js');
 const tape = require('tape');
 const Block = require('../lib/block.js');
-const common = require('ethereum-common');
 const VM = require('../lib/vm/index.js');
 
 var trie = new Trie();
@@ -10,7 +9,7 @@ var trie = new Trie();
 tape('[Common]: genesis hashes tests', function(t) {
   t.test('should generate the genesis state correctly', function(st) {
     var vm = new VM(trie);
-    vm.generateGenesis(common.allotments , function() {
+    vm.generateCanonicalGenesis(function() {
       st.equal(vm.trie.root.toString('hex'), genesisData.genesis_state_root);
       st.end();
     });
@@ -18,7 +17,8 @@ tape('[Common]: genesis hashes tests', function(t) {
 
   t.test('should generete the genesis correctly', function(st) {
     var blockGenesis = new Block();
-    blockGenesis.header.stateRoot = genesisData.genesis_state_root;
+    blockGenesis.header.stateRoot = trie.root
+    console.log(trie.root.toString('hex'));
     var rlpGenesis = blockGenesis.serialize();
     st.strictEqual(rlpGenesis.toString('hex'),
       genesisData.genesis_rlp_hex, 'rlp hex mismatch');
