712c0656805f4108bb7e417cd6ccb27f7fa64d11
wanderer <wanderer@users.noreply.github.com>
2015-08-19 23:59:24 +0000

    Delete eslint.sh

--- script/eslint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# run eslint on modified js files
-git diff $(git merge-base HEAD origin/HEAD) --name-only --diff-filter=ACM | grep -v node_modules | grep ".js$" | xargs node_modules/.bin/eslint -c ./script/eslint.json
